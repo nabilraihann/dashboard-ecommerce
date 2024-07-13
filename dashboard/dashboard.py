da9cc29f80ac68a1d2e1ef6978f9657c7b51b4ba
Nabil Muhammad Raihan <namura152@gmail.com>
2024-07-13 10:34:19 +0700

    change file path

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -111,7 +111,7 @@ def create_map_df(df):
     return map_df
 
 
-all_df = pd.read_csv("./all_data.csv")
+all_df = pd.read_csv("../dashboard/all_data.csv")
 
 
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
